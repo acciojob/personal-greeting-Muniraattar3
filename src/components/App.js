4b1f24b8d7612e7383120c73d1445a19856a3afb
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-04-12 21:40:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import React,{useState} from "react";
 import './../styles/App.css';
 
-const App=()=>{
+const App = () => {
   const [input,setInput] = useState('');
-
-  const onInput=(e)=>{
-    const {value} = e.target
+  const onInput = (e)=>{
+      const {value} = e.target
     setInput(value)
   }
-
-  const onClear()=>{
-    setInput('');
-}
-  return(
+  const onClear = ()=>{
+    setInput('')
+  }
+  
+  return (
     <div>
-     <p>Enter your name:</p>
-  <input value={input} onChange ={onInput}/>
-    {!!input.length && <p>Hello {input}!</p>}
-     <button onClick={onCLear}> Clear</button>
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
-    )
+  )
 }
+
 export default App
